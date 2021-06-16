bffec98a12c1f4bdd2f274b667fec4efe6083852
cgonzalezh333 <claudiogh33@gmail.com>
2021-06-15 22:40:05 -0600

    landing page 75 complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'HomePageGoes',component:HomePageComponent},
+
+  {path:'**',redirectTo:'HomePageGoes'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
