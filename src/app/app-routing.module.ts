df933ec41a8a8a19d7e75a907ee73a757c9bb07d
cgonzalezh333 <claudiogh33@gmail.com>
2021-07-14 22:34:53 -0600

    podcast page implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { CentroAyudaComponent } from './centro-ayuda/centro-ayuda.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NoticiasComponent } from './noticias/noticias.component';
 import { OficinaVirtualComponent } from './oficina-virtual/oficina-virtual.component';
+import { PodcastComponent } from './podcast/podcast.component';
 import { SellosVerdesComponent } from './sellos-verdes/sellos-verdes.component';
 import { VitrinaComponent } from './vitrina/vitrina.component';
 
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'Oficinas-Virtuales', component: OficinaVirtualComponent },
   { path: 'Vitrina-Estudiantil', component: VitrinaComponent },
   { path: 'Sellos-Verdes', component: SellosVerdesComponent },
+  { path: 'Podcast-Goes',component:PodcastComponent},
   { path: '**', redirectTo: 'Inicio' },
 ];
 
